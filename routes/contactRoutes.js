1f49c3d6c91734d98e069afcd405209fa941abb1
Bruno <nweremizu.bruno@stu.cu.edu.ng>
2024-07-23 13:38:21 +0100

    refactor: Update contact routes to use "contact" instead of "contacts" in URL paths

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -5,50 +5,50 @@ const { validateToken } = require("../middlewares/validateToken");
 const router = express.Router();
 
 router
-  .route("/:churchId/contacts")
+  .route("/:churchId")
   .get(validateToken, contactController.getContacts)
   .post(validateToken, contactController.createContact);
 
 router
-  .route("/:churchId/contacts/:contactId")
+  .route("/:churchId/contact/:contactId")
   .get(validateToken, contactController.getContact)
   .patch(validateToken, contactController.updateContact)
   .delete(validateToken, contactController.deleteContact);
 
 router
-  .route("/:churchId/contacts/:contactId/status")
+  .route("/:churchId/contact/:contactId/status")
   .get(validateToken, contactController.changeStatus);
 
 router
-  .route("/:churchId/contacts/:contactId/notes")
+  .route("/:churchId/contact/:contactId/notes")
   .get(validateToken, contactController.getNotes)
   .post(validateToken, contactController.createNote);
 
 router
-  .route("/:churchId/contacts/:contactId/notes/:noteId")
+  .route("/:churchId/contact/:contactId/notes/:noteId")
   .patch(validateToken, contactController.updateNote)
   .delete(validateToken, contactController.deleteNote);
 
 router
-  .route("/:churchId/contacts/:contactId/assign/:memberId")
+  .route("/:churchId/contact/:contactId/assign/:memberId")
   .post(validateToken, contactController.assignContact)
   .delete(validateToken, contactController.unassignContact);
 
 router
-  .route("/:churchId/contacts/:contactId/label")
+  .route("/:churchId/contact/:contactId/label")
   .post(validateToken, contactController.addLabel)
   .get(validateToken, contactController.getLabels);
 
 router
-  .route("/:churchId/contacts/:contactId/label/:labelId")
+  .route("/:churchId/contact/:contactId/label/:labelId")
   .delete(validateToken, contactController.removeLabel);
 
 //action
 router
-  .route("/:churchId/contacts/:contactId/action")
+  .route("/:churchId/contact/:contactId/action")
   .post(validateToken, contactController.addAction);
 
 router
-  .route("/:churchId/contacts/:contactId/action/:actionId")
+  .route("/:churchId/contact/:contactId/action/:actionId")
   .patch(validateToken, contactController.updateAction);
 module.exports = router;
