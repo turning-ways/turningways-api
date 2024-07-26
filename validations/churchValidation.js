c02890fc2245c80a1914b1ad6b3d2ecf811ebbb6
Bruno <nweremizu.bruno@stu.cu.edu.ng>
2024-07-26 23:16:44 +0100

    refactor: Separate churchData and memberData in churchOnBoardingValidation

--- validations/churchValidation.js
+++ validations/churchValidation.js
@@ -2,24 +2,45 @@ const { body } = require("express-validator");
 const validateRequest = require("../middlewares/validateRequests");
 
 exports.churchOnBoardingValidation = [
-  body("name").notEmpty().withMessage("Name is required"),
-  body("address").notEmpty().withMessage("Address is required"),
-  body("email").isEmail().withMessage("Valid email is required"),
-  body("phone").notEmpty().withMessage("Phone number is required"),
-  body("website").optional().isURL().withMessage("Valid URL is required"),
-  body("city").notEmpty().withMessage("City is required"),
-  body("state").notEmpty().withMessage("State is required"),
-  body("country").notEmpty().withMessage("Country is required"),
-  body("postalCode").notEmpty().withMessage("Postal code is required"),
-  body("logo").optional().isURL().withMessage("Valid URL is required"),
-  body("hasParentChurch")
+  body("churchData.name").notEmpty().withMessage("Name is required"),
+  body("churchData.address").notEmpty().withMessage("Address is required"),
+  body("churchData.email").isEmail().withMessage("Valid email is required"),
+  body("churchData.phone").notEmpty().withMessage("Phone number is required"),
+  body("churchData.website")
+    .optional()
+    .isURL()
+    .withMessage("Valid URL is required"),
+  body("churchData.city").notEmpty().withMessage("City is required"),
+  body("churchData.state").notEmpty().withMessage("State is required"),
+  body("churchData.country").notEmpty().withMessage("Country is required"),
+  body("churchData.postalCode")
+    .notEmpty()
+    .withMessage("Postal code is required"),
+  body("churchData.logo")
+    .optional()
+    .isURL()
+    .withMessage("Valid URL is required"),
+  body("churchData.hasParentChurch")
     .isBoolean()
     .withMessage("hasParentChurch must be a boolean"),
-  body("level").optional().isString().withMessage("Level ID must be a string"),
-  body("parentChurch")
+  body("churchData.level")
+    .optional()
+    .isString()
+    .withMessage("Level ID must be a string"),
+  body("churchData.parentChurch")
     .optional()
     .isString()
     .withMessage("Parent Church must be a string"),
+
+  body("memberData.phone").notEmpty().withMessage("Phone number is required"),
+  body("memberData.howDidYouHear")
+    .notEmpty()
+    .withMessage("How did you hear about us is required"),
+  body("memberData.email").isEmail().withMessage("Valid email is required"),
+  body("memberData.gender").notEmpty().withMessage("Gender is required"),
+  body("memberData.dateOfBirth")
+    .notEmpty()
+    .withMessage("Date of Birth is required"),
   validateRequest,
 ];
 
